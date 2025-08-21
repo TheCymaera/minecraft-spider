29bfc29d6946653a5ca3b62745a6eed02e595670
Pookachu <mc.astradal@gmail.com>
2025-08-21 18:18:11 -0500

    Fix bug, skips legacy materials

--- src/main/java/com/heledron/spideranimation/utilities/blockColorLookUp.kt
+++ src/main/java/com/heledron/spideranimation/utilities/blockColorLookUp.kt
@@ -9,84 +9,86 @@ import kotlin.math.sqrt
 
 private fun String.parseJSONColors(): Map<Material, Color> {
     val colorMap = mutableMapOf<Material, Color>()
-
     @Suppress("UNCHECKED_CAST")
     val json = Gson().fromJson(this, Map::class.java) as Map<String, List<Int>>
-
     for ((key, value) in json) {
         val material = Material.matchMaterial("minecraft:$key") ?: continue
         colorMap[material] = Color.fromRGB(value[0], value[1], value[2])
     }
-
     return colorMap
 }
 
-private val blocks = currentPlugin.getResource("block_colors.json")
-    ?.bufferedReader()
-    ?.use { it.readText() }
-    ?.parseJSONColors()
-    ?: throw IllegalStateException("Failed to load block_colors.json")
+private val blocks by lazy {
+    currentPlugin.getResource("block_colors.json")
+        ?.bufferedReader()
+        ?.use { it.readText() }
+        ?.parseJSONColors()
+        ?: throw IllegalStateException("Failed to load block_colors.json")
+}
 
-private val blocksWithBrightness = mutableMapOf<Color, Pair<Material,Int>>().apply {
-    for (brightness in 15 downTo 0) {
-        for ((material, color) in blocks) {
-            if (!material.isOccluding) continue
-
-            val newColor = color.withBrightness(brightness)
-
-            if (newColor in this) continue
-
-            this[newColor] = material to brightness
+private val blocksWithBrightness by lazy {
+    mutableMapOf<Color, Pair<Material,Int>>().apply {
+        for (brightness in 15 downTo 0) {
+            for ((material, color) in blocks) {
+                if (!material.isOccluding) continue
+                val newColor = color.withBrightness(brightness)
+                if (newColor in this) continue
+                this[newColor] = material to brightness
+            }
         }
-    }
 
-
-    for ((color, material) in this) {
-        val id = material.first.key.toString()
-
-        // replace log with wood
-        if (id.endsWith("_log")) {
-            val woodMaterial = Material.matchMaterial(id.replaceEnd("_log", "_wood")) ?: continue
-            this[color] = woodMaterial to material.second
+        // This inner loop is modifying the map while iterating, which can be risky.
+        // Creating a list of changes to apply after is safer.
+        val changes = mutableMapOf<Color, Pair<Material, Int>>()
+        for ((color, material) in this) {
+            val id = material.first.key.toString()
+            if (id.endsWith("_log")) {
+                val woodMaterial = Material.matchMaterial(id.replace("_log", "_wood"))
+                if (woodMaterial != null) {
+                    changes[color] = woodMaterial to material.second
+                }
+            }
         }
+        this.putAll(changes)
     }
 }
 
-private val blockToColor = blocks.toMutableMap().apply {
-    this[Material.GRASS_BLOCK] = this[Material.MOSS_BLOCK]!!
-    this[Material.MOSS_CARPET] = this[Material.MOSS_BLOCK]!!
-    this[Material.OAK_LEAVES] = this[Material.MOSS_BLOCK]!!
+private val blockToColor by lazy {
+    blocks.toMutableMap().apply {
+        this[Material.GRASS_BLOCK] = this[Material.MOSS_BLOCK]!!
+        this[Material.MOSS_CARPET] = this[Material.MOSS_BLOCK]!!
+        this[Material.OAK_LEAVES] = this[Material.MOSS_BLOCK]!!
+        this[Material.WARPED_TRAPDOOR] = this[Material.WARPED_PLANKS]!!
+        this[Material.BARREL] = this[Material.SPRUCE_PLANKS]!!
 
-    this[Material.WARPED_TRAPDOOR] = this[Material.WARPED_PLANKS]!!
+        for (material in Material.entries) {
+            if (material.isLegacy) continue
 
-    this[Material.BARREL] = this[Material.SPRUCE_PLANKS]!!
+            if (!material.isBlock) continue
+            val id = material.key.toString()
 
-    // replace partial blocks with their full block counterparts
-    for (material in Material.entries) {
-        if (!material.isBlock) continue
-        val id = material.key.toString()
+            if (this.containsKey(material)) continue
 
-        if (this.containsKey(material)) continue
+            val fullBlockName = id
+                .replaceEnd("_slab", "")
+                .replaceEnd("_stairs", "")
+                .replaceEnd("_wall", "")
+                .replaceEnd("_trapdoor", "")
+                .replace("waxed_", "")
 
-        val fullBlockName = id
-            .replaceEnd("_slab", "")
-            .replaceEnd("_stairs", "")
-            .replaceEnd("_wall", "")
-            .replaceEnd("_trapdoor", "")
-            .replace("waxed_", "")
+            if (fullBlockName == id) continue
 
-        if (fullBlockName == id) continue
+            val fullBlockMaterial =
+                Material.matchMaterial(fullBlockName + "_planks") ?:
+                Material.matchMaterial(fullBlockName) ?:
+                Material.matchMaterial(fullBlockName + "s") ?:
+                Material.matchMaterial(fullBlockName + "_wood")
 
-        val fullBlockMaterial =
-            Material.matchMaterial(fullBlockName + "_planks") ?:
-            Material.matchMaterial(fullBlockName) ?:
-            Material.matchMaterial(fullBlockName + "s") ?:
-            Material.matchMaterial(fullBlockName + "_wood")
+            this[material] = this[fullBlockMaterial] ?: continue
+        }
 
-        this[material] = this[fullBlockMaterial] ?: continue
+        this[Material.CAMPFIRE] = this[Material.OAK_LOG]!!
     }
-
-    this[Material.CAMPFIRE] = this[Material.OAK_LOG]!!
 }
 
 fun getColorFromBlock(block: BlockData): Color? {
@@ -106,7 +108,6 @@ class MatchInfo(
 
 fun getBestMatchFromColor(color: Color, allowCustomBrightness: Boolean): MatchInfo {
     val map = if (allowCustomBrightness) blocksWithBrightness else blocksWithBrightness.filterValues { it.second == 15 }
-
     val bestMatch = map.minBy { it.key.distanceTo(color) }
     return MatchInfo(bestMatch.value.first.createBlockData(), bestMatch.key, color.distanceTo(bestMatch.key), bestMatch.value.second)
 }
